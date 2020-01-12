7d1d65e3146ceb95783c37b1936f0a826d54e36c
osinskja <49365608+osinskja@users.noreply.github.com>
2020-01-12 16:02:46 +0100

    first working web app

---
+++ src/main/java/com/adao/codility/solutions/AdaoCodilitySolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.adao.codility.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"controller"})
+public class AdaoCodilitySolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaoCodilitySolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adao/codility/solutions/AdaoCodilitySolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adao.codility.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaoCodilitySolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
