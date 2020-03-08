b8245e23f19a4bc58a5f215ee2a972ade925c8ee
osinskja <osinskja@gmail.com>
2020-03-08 15:04:52 +0100

    adding datasource and prod profile

--- front/src/main/java/controller/HomeController.java
+++ front/src/main/java/controller/HomeController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String homePage(){
+        jdbcTemplate.execute("DROP TABLE books IF EXISTS");
         return "homePage";
     }
 }
