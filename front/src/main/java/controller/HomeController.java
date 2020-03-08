e6f11890d797ab7e852cac3201f0c825e766b414
osinskja <osinskja@gmail.com>
2020-03-08 18:04:00 +0100

    error 404 fix

--- front/src/main/java/controller/HomeController.java
+++ front/src/main/java/controller/HomeController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
