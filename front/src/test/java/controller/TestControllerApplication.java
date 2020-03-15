712fbbe75315a1f51f3be458d2d362ec1255fe5f
osinskja <osinskja@gmail.com>
2020-03-15 22:20:44 +0100

    local database setup

--- front/src/test/java/controller/TestControllerApplication.java
+++ front/src/test/java/controller/TestControllerApplication.java
@@ -1,7 +1,15 @@
 package controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootApplication
 public class TestControllerApplication {
+    @Bean
+    JdbcTemplate jdbcTemplate(){
+        return mock(JdbcTemplate.class);
+    }
 }
