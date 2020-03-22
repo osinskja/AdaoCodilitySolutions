bd927758ee287289dcd4b979980f9438e7f53224
osinskja <osinskja@gmail.com>
2020-03-22 20:56:31 +0100

    modifying tests for controller

--- front/src/test/java/controller/TestControllerApplication.java
+++ front/src/test/java/controller/TestControllerApplication.java
@@ -1,8 +1,15 @@
 package controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import repository.template.SolutionRepository;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootApplication
 public class TestControllerApplication {
-
+    @Bean
+    public SolutionRepository solutionRepository(){
+        return mock(SolutionRepository.class);
+    }
 }
